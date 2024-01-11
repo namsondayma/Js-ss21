8a9b67214a6e036c9595b832f1f2293e96de6cae
namsondayma <147359790+namsondayma@users.noreply.github.com>
2024-01-11 14:42:52 +0700

    Add files via upload

---
+++ ss21/index.js
@@ -0,0 +1,18 @@
+
+//bai1
+var age = +prompt("Ban bao nhieu tuoi:");
+if(0 < age && age < 120){
+ console.log("tuoi cua ban la:",age);
+}else{
+    console.log("tuoi cua ban khong hop le");
+}
+//bai2
+var number = +prompt("so ban nhap la:");
+if(number % 2 === 0){
+    console.log("day la so chan");
+}else if(number % 2 === 1){
+    console.log("day la so le");
+}else{
+    console.log("so khong hop le");
+}
+//bai3 
